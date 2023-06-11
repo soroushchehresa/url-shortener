6ee188b6ad1e0ea01505f87255609ac4ea5eeb2b
Soroush Chehresa <s1996ch@gmail.com>
2023-06-12 00:18:06 +0200

    Update next.config.js

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
